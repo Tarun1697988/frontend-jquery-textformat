106cce16fdb62005421ad9d2f29324fc653dbcdd
Tarun1697988 <42170692+Tarun1697988@users.noreply.github.com>
2018-11-26 19:11:57 +0200

    fix code for lint

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import './styles/style.css';
-import $ from 'jquery';
 
 const textAreaIn = document.querySelector('.message-field');
 const textAreaOut = document.querySelector('.message-formatted');
 const resetButton = document.querySelector('.reset-button');
 
-textAreaIn.addEventListener('input', function () { 
-  textAreaOut.innerHTML = textAreaIn.value.toLowerCase().trim().replace(/\s+/g, ' ').replace(/\n/g, ' ');
+
+textAreaIn.addEventListener('input', function () {
+	textAreaOut.innerHTML = textAreaIn.value.toLowerCase().trim().replace(/\s+/g, ' ').replace(/\n/g, ' ');
 });
 
-resetButton.addEventListener('click', function () { 
-  textAreaIn.value = '';
-  textAreaOut.innerHTML = '';
+resetButton.addEventListener('click', function () {
+	textAreaIn.value = '';
+	textAreaOut.innerHTML = '';
 });
